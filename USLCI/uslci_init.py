0bc245ca3ab3d0043fa5bb0c024a63323ea54f33
jianchuanqi <qijianchuan@hotmail.com>
2021-12-15 23:35:31 +0800

    Table names begin with "uslci_". Open files encoding utf8.

--- USLCI/uslci_init.py
+++ USLCI/uslci_init.py
@@ -17,7 +17,7 @@ def mergeJson(base):
     # merge jsons in a dir
     dfs = []
     for i in findAllFile(base):
-        with open(i) as f:
+        with open(i,encoding='utf8') as f:
             data = pd.json_normalize(json.load(f))
         dfs.append(data)
     df = pd.concat(dfs)
@@ -31,23 +31,23 @@ db_conn = create_engine(
 actors = "USLCI/data/actors"
 df = mergeJson(actors)
 df.drop(["@context"], axis=1, inplace=True)
-df.to_sql("actors", con=db_conn, if_exists="append", index=None)
-db_conn.execute('ALTER TABLE actors ADD CONSTRAINT "actors_key" PRIMARY KEY ("@id");')
+df.to_sql("uslci_actors", con=db_conn, if_exists="append", index=None)
+db_conn.execute('ALTER TABLE uslci_actors ADD CONSTRAINT "uslci_actors_key" PRIMARY KEY ("@id");')
 
 categories = "USLCI/data/categories"
 df = mergeJson(categories)
 df.drop(["@context"], axis=1, inplace=True)
-df.to_sql("categories", con=db_conn, if_exists="append", index=None)
+df.to_sql("uslci_categories", con=db_conn, if_exists="append", index=None)
 db_conn.execute(
-    'ALTER TABLE categories ADD CONSTRAINT "categories_key" PRIMARY KEY ("@id");'
+    'ALTER TABLE uslci_categories ADD CONSTRAINT "uslci_categories_key" PRIMARY KEY ("@id");'
 )
 
 currencies = "USLCI/data/currencies"
 df = mergeJson(currencies)
 df.drop(["@context"], axis=1, inplace=True)
-df.to_sql("currencies", con=db_conn, if_exists="append", index=None)
+df.to_sql("uslci_currencies", con=db_conn, if_exists="append", index=None)
 db_conn.execute(
-    'ALTER TABLE currencies ADD CONSTRAINT "currencies_key" PRIMARY KEY ("@id");'
+    'ALTER TABLE uslci_currencies ADD CONSTRAINT "uslci_currencies_key" PRIMARY KEY ("@id");'
 )
 
 dq_systems = "USLCI/data/dq_systems"
@@ -55,22 +55,22 @@ df = mergeJson(dq_systems)
 df["indicators"] = list(map(lambda x: json.dumps(x), df["indicators"]))
 df.drop(["@context"], axis=1, inplace=True)
 df.to_sql(
-    "dq_systems",
+    "uslci_dq_systems",
     con=db_conn,
     if_exists="append",
     index=None,
     dtype={"indicators": sqlalchemy.types.JSON},
 )
 db_conn.execute(
-    'ALTER TABLE dq_systems ADD CONSTRAINT "dq_systems_key" PRIMARY KEY ("@id");'
+    'ALTER TABLE uslci_dq_systems ADD CONSTRAINT "uslci_dq_systems_key" PRIMARY KEY ("@id");'
 )
 
 flow_properties = "USLCI/data/flow_properties"
 df = mergeJson(flow_properties)
 df.drop(["@context"], axis=1, inplace=True)
-df.to_sql("flow_properties", con=db_conn, if_exists="append", index=None)
+df.to_sql("uslci_flow_properties", con=db_conn, if_exists="append", index=None)
 db_conn.execute(
-    'ALTER TABLE flow_properties ADD CONSTRAINT "flow_properties_key" PRIMARY KEY ("@id");'
+    'ALTER TABLE uslci_flow_properties ADD CONSTRAINT "uslci_flow_properties_key" PRIMARY KEY ("@id");'
 )
 
 flows = "USLCI/data/flows"
@@ -78,20 +78,20 @@ df = mergeJson(flows)
 df["flowProperties"] = list(map(lambda x: json.dumps(x), df["flowProperties"]))
 df.drop(["@context"], axis=1, inplace=True)
 df.to_sql(
-    "flows",
+    "uslci_flows",
     con=db_conn,
     if_exists="append",
     index=None,
     dtype={"flowProperties": sqlalchemy.types.JSON},
 )
-db_conn.execute('ALTER TABLE flows ADD CONSTRAINT "flows_key" PRIMARY KEY ("@id");')
+db_conn.execute('ALTER TABLE uslci_flows ADD CONSTRAINT "uslci_flows_key" PRIMARY KEY ("@id");')
 
 locations = "USLCI/data/locations"
 df = mergeJson(locations)
 df.drop(["@context"], axis=1, inplace=True)
-df.to_sql("locations", con=db_conn, if_exists="append", index=None)
+df.to_sql("uslci_locations", con=db_conn, if_exists="append", index=None)
 db_conn.execute(
-    'ALTER TABLE locations ADD CONSTRAINT "locations_key" PRIMARY KEY ("@id");'
+    'ALTER TABLE uslci_locations ADD CONSTRAINT "uslci_locations_key" PRIMARY KEY ("@id");'
 )
 
 processes = "USLCI/data/processes"
@@ -104,7 +104,7 @@ df["exchanges"] = list(map(lambda x: json.dumps(x), df["exchanges"]))
 df["allocationFactors"] = list(map(lambda x: json.dumps(x), df["allocationFactors"]))
 df.drop(["@context"], axis=1, inplace=True)
 df.to_sql(
-    "processes",
+    "uslci_processes",
     con=db_conn,
     if_exists="append",
     index=None,
@@ -115,26 +115,26 @@ df.to_sql(
     },
 )
 db_conn.execute(
-    'ALTER TABLE processes ADD CONSTRAINT "processes_key" PRIMARY KEY ("@id");'
+    'ALTER TABLE uslci_processes ADD CONSTRAINT "uslci_processes_key" PRIMARY KEY ("@id");'
 )
 
 sources = "USLCI/data/sources"
 df = mergeJson(sources)
 df.drop(["@context"], axis=1, inplace=True)
-df.to_sql("sources", con=db_conn, if_exists="append", index=None)
-db_conn.execute('ALTER TABLE sources ADD CONSTRAINT "sources_key" PRIMARY KEY ("@id");')
+df.to_sql("uslci_sources", con=db_conn, if_exists="append", index=None)
+db_conn.execute('ALTER TABLE uslci_sources ADD CONSTRAINT "uslci_sources_key" PRIMARY KEY ("@id");')
 
 unit_groups = "USLCI/data/unit_groups"
 df = mergeJson(unit_groups)
 df["units"] = list(map(lambda x: json.dumps(x), df["units"]))
 df.drop(["@context"], axis=1, inplace=True)
 df.to_sql(
-    "unit_groups",
+    "uslci_unit_groups",
     con=db_conn,
     if_exists="append",
     index=None,
     dtype={"units": sqlalchemy.types.JSON},
 )
 db_conn.execute(
-    'ALTER TABLE unit_groups ADD CONSTRAINT "unit_groups_key" PRIMARY KEY ("@id");'
+    'ALTER TABLE uslci_unit_groups ADD CONSTRAINT "uslci_unit_groups_key" PRIMARY KEY ("@id");'
 )
